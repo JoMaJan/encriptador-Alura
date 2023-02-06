f3628dff71e32aa29d10406bb84b760c69ebe6eb
Jose Janin <josemajanin@gmail.com>
2023-02-06 17:03:32 -0300

    Boton desencriptado listo.

--- functions.js
+++ functions.js
@@ -4,30 +4,39 @@
 function encriptar(){
     let mensaje = document.getElementById("inputMensaje").value;
     let mensajeEncriptado = mensaje.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i(?=a)/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
+    const caja = document.getElementById("cajaTexto");
+    const caja2 = document.getElementById("cajaCodificado");
     console.log(mensajeEncriptado);  
-    mostrarCaja(mensajeEncriptado);
+    mostrarCaja(mensajeEncriptado, caja, caja2);
 
 }
 
-function mostrarCaja(textoClave){
-    let caja = document.getElementById("cajaTexto");
-    /*caja.style.animation = "fade-out-bottom 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) 0.5s both";*/
-    caja.style.display = "none";
+function mostrarCaja(textoClave, cajon1, cajon2){   
     
-    let caja2 = document.getElementById("cajaCodificado");
-    caja2.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
-    caja2.style.display = "block";
+    cajon1.style.display = "none";    
+    cajon2.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
+    cajon2.style.display = "block";
     document.getElementById("inputMensaje2").value = textoClave;
 
 }
 
+
 function desencriptar(){
-    texto = document.getElementById("inputMensaje").value;
-    var textoEncriptado = texto.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
+    let texto = document.getElementById("inputMensaje").value;
+    let textoEncriptado = texto.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
+    const caja = document.getElementById("cajaTexto");
+    const caja2 = document.getElementById("cajaCodificado");
     console.log(textoEncriptado);
-    
+    ocultarCaja(textoEncriptado,caja, caja2);  
 }
 
+
+function ocultarCaja(palabras, parte1, parte2){
+    parte2.style.display = "none";
+    parte1.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
+    parte1.style.display = "block";
+    document.getElementById("inputMensaje").value = palabras;
+}
 
 
 
