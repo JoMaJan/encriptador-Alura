de61b1bae9d97485f683dfc8db4e42979fca308b
Jose Janin <josemajanin@gmail.com>
2023-02-06 16:15:12 -0300

    Boton encriptado listo.

--- functions.js
+++ functions.js
@@ -1,17 +1,23 @@
 
-var texto = "";
- 
-    
+
 
 function encriptar(){
-    texto = document.getElementById("inputMensaje").value;
-    var texto2 = texto.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i(?=a)/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
-    console.log(texto2);
+    let mensaje = document.getElementById("inputMensaje").value;
+    let mensajeEncriptado = mensaje.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i(?=a)/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
+    console.log(mensajeEncriptado);  
+    mostrarCaja(mensajeEncriptado);
 
-    document.getElementById("cajaImagen").innerHTML=
-        texto2;
-    document.getElementById("cajaImagen").style.border = 
-    ("2px solid black")
+}
+
+function mostrarCaja(textoClave){
+    let caja = document.getElementById("cajaTexto");
+    /*caja.style.animation = "fade-out-bottom 1s cubic-bezier(0.250, 0.460, 0.450, 0.940) 0.5s both";*/
+    caja.style.display = "none";
+    
+    let caja2 = document.getElementById("cajaCodificado");
+    caja2.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
+    caja2.style.display = "block";
+    document.getElementById("inputMensaje2").value = textoClave;
 
 }
 
@@ -19,7 +25,10 @@ function desencriptar(){
     texto = document.getElementById("inputMensaje").value;
     var textoEncriptado = texto.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
     console.log(textoEncriptado);
+    
 }
+
+
 
 
 
