32ed877c6a359c0c773bc89b246f0ca87c6de5f7
Jose Janin <josemajanin@gmail.com>
2023-02-07 12:06:40 -0300

    Cambie las funciones

--- functions.js
+++ functions.js
@@ -1,42 +1,32 @@
 
-
-
-function encriptar(){
+document.getElementById('botonEncriptar').addEventListener('click', function() {
     let mensaje = document.getElementById("inputMensaje").value;
     let mensajeEncriptado = mensaje.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i(?=a)/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
-    const caja = document.getElementById("cajaTexto");
-    const caja2 = document.getElementById("cajaCodificado");
-    console.log(mensajeEncriptado);  
-    mostrarCaja(mensajeEncriptado, caja, caja2);
-
-}
-
-function mostrarCaja(textoClave, cajon1, cajon2){   
-    
-    cajon1.style.display = "none";    
-    cajon2.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
-    cajon2.style.display = "block";
-    document.getElementById("inputMensaje2").value = textoClave;
-
-}
+    let caja = document.getElementById("cajaTexto");
+ 
+    caja.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both"; 
+    document.getElementById("inputMensaje").value = mensajeEncriptado;  
+});
 
 
-function desencriptar(){
+document.getElementById('botonDesencriptar').addEventListener('click', function() {
     let texto = document.getElementById("inputMensaje").value;
     let textoEncriptado = texto.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
-    const caja = document.getElementById("cajaTexto");
-    const caja2 = document.getElementById("cajaCodificado");
-    console.log(textoEncriptado);
-    ocultarCaja(textoEncriptado,caja, caja2);  
-}
+    let caja = document.getElementById("cajaTexto");
+
+    caja.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
+    document.getElementById("inputMensaje").value = textoEncriptado;
+});
 
 
-function ocultarCaja(palabras, parte1, parte2){
-    parte2.style.display = "none";
-    parte1.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
-    parte1.style.display = "block";
-    document.getElementById("inputMensaje").value = palabras;
-}
+document.getElementById('botonCopiar').addEventListener('click', function() {
+        textoCopiar = document.getElementById("inputMensaje").value;
+        navigator.clipboard.writeText(textoCopiar);
+})
+
+
+
+
 
 
 
