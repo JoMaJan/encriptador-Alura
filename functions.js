2fb7bff017ba458ff79f2255461afb636585965e
Jose Janin <josemajanin@gmail.com>
2023-02-07 20:11:03 -0300

    Corregí función encriptar y agregué función copiar y validar.

--- functions.js
+++ functions.js
@@ -1,14 +1,16 @@
-/*(?=a)*/
+/* function para encriptar el mensaje al presionar el boton encriptar*/
 
 document.getElementById('botonEncriptar').addEventListener('click', function() {
     let mensaje = document.getElementById("inputMensaje").value;
-    let mensajeEncriptado = mensaje.replace(/a/g,"ai").replace(/e/g,"enter").replace(/i(?=a)/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
+    let mensajeEncriptado = mensaje.replace(/a/g,"ai").replace(/e/g,"enter").replace(/(?<!a)i/g,"imes").replace(/o/g,"ober").replace(/u/g,"ufat");
     let caja = document.getElementById("cajaTexto");
  
     caja.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both"; 
-    document.getElementById("inputMensaje").value = mensajeEncriptado;  
+    document.getElementById("inputMensaje").value = mensajeEncriptado; 
+    validar(mensaje);
 });
 
+/* function para desencriptar el mensaje al presionar el boton desencriptar*/
 
 document.getElementById('botonDesencriptar').addEventListener('click', function() {
     let texto = document.getElementById("inputMensaje").value;
@@ -17,13 +19,34 @@ document.getElementById('botonDesencriptar').addEventListener('click', function(
 
     caja.style.animation = "fade-in-bottom 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) 0.5s both";
     document.getElementById("inputMensaje").value = textoEncriptado;
+    validar(textoEncriptado);
 });
+
+/* function para guardar el texto en el portapapeles al hacer click en su icono*/
 
 
 document.getElementById('botonCopiar').addEventListener('click', function() {
         textoCopiar = document.getElementById("inputMensaje").value;
         navigator.clipboard.writeText(textoCopiar);
 })
+
+/* function para validar que no haya mayusculas*/
+
+
+function validar(x){
+    for(var index = 0; index < x.length; index++){
+        var letraActual = x.charAt(index);
+        var espacio = " ";
+        if((esMayuscula(letraActual)) && (letraActual != espacio)){
+            alert("Ingresó una letra mayúscula, recuerde usar solo letras minúsculas.");
+            location.reload();
+        }
+    }
+}
+
+function esMayuscula(letra){
+    return letra == letra.toUpperCase();
+}
 
 
 
